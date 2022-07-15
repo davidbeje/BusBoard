dc7852875cf436251445ee491780caf504c64216
davidbeje <david.bejenariu@softwire.com>
2022-07-15 17:28:41 +0300

    first commit

---
+++ BusBoard.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace BusBoard.ConsoleApp
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("https://api.tfl.gov.uk");
+
+            var request = new RestRequest("StopPoint/Mode/bus/Disruption");
+
+            var response = client.Execute(request).Content;
+
+            Console.WriteLine(response);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BusBoard/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BusBoard.MvcApplication" Language="C#" %>
